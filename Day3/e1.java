252774b0bb6cf562256b04fe88f23e922b74b904
Nity05 <csnithishk05@gmail.com>
2024-07-28 12:04:52 +0530

    Created e1.java

---
+++ Day3/e1.java
@@ -0,0 +1,8 @@
+public class e1{
+    public static void main(String args[]){
+    String firstName="John";
+    String lastname="Smith";
+    System.out.println(firstName.concat(lastname));
+    }
+}
+    
