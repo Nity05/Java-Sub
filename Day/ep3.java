d41855bbca953b801590d637c281de22ce5bac05
Nity05 <146318387+Nity05@users.noreply.github.com>
2024-07-28 12:08:47 +0530

    Delete Day directory

--- Day/ep3.java
+++
@@ -1,7 +0,0 @@
-public class ep3{
-public static void main(String args[]){
-String txt="We are the so-called \"Vikings\" from the north";
-
-System.out.println(txt);
-}
-}
--- Day/ep4.java
+++
@@ -1,5 +0,0 @@
-public class ep4{
-public static void main(String args[]){
-System.out.println(Math.max(200000,90));
-}
-}
