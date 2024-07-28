d6f1349aa83e336cde2da1375533168333be5a70
Nity05 <csnithishk05@gmail.com>
2024-07-28 11:09:34 +0530

    Created Day1 Var.java

---
+++ Day1/Var.java
@@ -0,0 +1,14 @@
+public class Var {
+
+        public static void main(String[] args){
+           final int a=12;
+           int b=10;
+           b+=25;
+           String s="Hello world";
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(s);
+        }
+    }
+    
+
