163abef6d2d2aca5f67c41e970a3c4aabdf0b160
Nity05 <csnithishk05@gmail.com>
2024-07-28 11:02:49 +0530

    Created Day1

---
+++ Day1/Comm.java
@@ -0,0 +1,17 @@
+public class Comm{
+    public static void main(String[] args){
+       final int a=12;
+       int b=10;
+       b+=25;
+       //This is a single line comment
+
+       /*This comment can be of multi line
+         Line 1
+         Line 2
+        */
+       String s="Hello world";
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(s);
+    }
+}
