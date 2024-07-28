5c57c904363dbbe5514ba0e5bf76025244c11efc
Nity05 <csnithishk05@gmail.com>
2024-07-28 16:23:08 +0530

    Created Day4

--- Day4/ep1.java
+++ Day4/ep1.java
@@ -2,12 +2,6 @@ public class ep1{
 public static void main(String args[]){
 String firstName="John";
 String lastname="Smith";
-<<<<<<< HEAD
 System.out.println(firstName+" "+lastname);
 }
 }
-=======
-System.out.println(firstName+" "+lastName);
-}
-}
->>>>>>> ce479f1f9b1216ccf6d54b5211deb65ec01fb212
