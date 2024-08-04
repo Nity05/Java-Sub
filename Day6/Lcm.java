fc3dee4ca52bc10c2e6242945dadbb1e8f4bc3f1
Nity05 <csnithishk05@gmail.com>
2024-08-04 17:51:40 +0530

    Updated Lcm.java

--- Day6/Lcm.java
+++ Day6/Lcm.java
@@ -6,12 +6,12 @@ public class Lcm{
         int n1=s.nextInt();        
         System.out.print("Enter the second number: ");
         int n2=s.nextInt();
-        int r=0;
-        r+=Math.max(n1,n2);
+        int r=Math.max(n1,n2);
         while (true) {
             if((r%n1==0)&&(r%n2==0)){
-                break;
-            r++;}
+                break;}
+            r++;
+        
         }
         System.out.println("The LCM of the numbers "+n1+" and "+n2+" is: "+r);
     }
